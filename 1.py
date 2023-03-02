b26b1357aa99072953227d8a7af8e707427f463b
Sabian519 <81668223+Sabian519@users.noreply.github.com>
2023-03-02 20:33:02 +0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -5,14 +5,8 @@ import random
 import sys
 
 def usage():
-    print "dP                                dP        dP                    a88888b.                dP           888888ba           dP     dP"
-    print "88                                88        88                   d8'   `88                88           88    `8b          88     88                    "
-    print "88        .d8888b. 88d888b. .d888b88        88 .d8888b. dP    dP 88        .d8888b. .d888b88 .d8888b. a88aaaa8P' dP    dP 88aaaaa88a .d8888b. 88d888b."
-    print "88        88'  `88 88'  `88 88'  `88        88 88'  `88 88    88 88        88'  `88 88'  `88 88ooood8  88   `8b. 88    88 88     88  88'  `88 88'  `88"
-    print "88        88.  .88 88       88.  .88 88.  .d8P 88.  .88 88.  .88 Y8.   .88 88.  .88 88.  .88 88.  ...  88    .88 88.  .88 88     88  88.  .88 88    88"
-    print "88888888P `88888P' dP       `88888P8  `Y8888'  `88888P8 `8888P88  Y88888P' `88888P' `88888P8 `88888P'  88888888P `8888P88 dP     dP  `88888P8 dP    dP"
-    print "ooooooooooooooooooooooooooooooooooooooooooooooooooooooooo~~~~.88~ooooooooooooooooooooooooooooooooooooooooooooooooo~~~~.88~ooooooooooooooooooooooooooooo"
-    print "#   LinkkDiscord:https://discord.gg/9KPqzSzNq9 \033[1;32m #"
+    print " gilbertneedcafein "
+    print "# KOMINFO KONTOLLLL"
 
 def flood(victim, vport, duration):
     # okay so here I create the server, when i say "SOCK_DGRAM" it means it's a UDP type program
